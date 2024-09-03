80f5b0790fd34511d8bafc921cff738bb4699a92
Ananya Gupta <ananya.gupta@volkswagen.co.in>
2024-09-03 20:06:05 +0530

    Outputting random values

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import LOGO from "./assets/react-core-concepts.png"
+import LOGO from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
 
+const terms = ["Fundamental", "Crucial", "Core"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
-
-function CoreConcept() {
+function CoreConcept(props) {
   return (
     <li>
-      <img src="" alt="" className="src" />
-      <h3>TITLE</h3>
-      <p>Description</p>
+      <img src={props.image} alt={props.title} className="src" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
     </li>
-  )
+  );
 }
 
 function App() {
@@ -17,20 +21,28 @@ function App() {
     <div>
       <header>
         <img src={LOGO} alt="Stylized atom" />
-        <h1>React Essentials</h1>
+        <h1>Ananya's React Notes</h1>
         <p>
-          Fundamental React concepts needed for almost any app you are
-          going to build!
+          {terms[getRandomInt(3)]} React concepts needed for almost any app you are going to
+          build!
         </p>
       </header>
       <main>
-        <section id = "core-concepts">
+        <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept title = "Components" description = "The core UI building block"/>
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
