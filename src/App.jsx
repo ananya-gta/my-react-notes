2c02dfa2eb24391fb6567b30c9c58ba8f8b987be
Ananya Gupta <ananya.gupta@volkswagen.co.in>
2024-09-03 15:54:38 +0530

    Added new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import LOGO from "./assets/react-core-concepts.png"
 
+
+
+function CoreConcept() {
+  return (
+    <li>
+      <img src="" alt="" className="src" />
+      <h3>TITLE</h3>
+      <p>Description</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
@@ -7,12 +19,20 @@ function App() {
         <img src={LOGO} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          Fundamental React concepts needed for almost any app you are
           going to build!
         </p>
       </header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id = "core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title = "Components" description = "The core UI building block"/>
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
